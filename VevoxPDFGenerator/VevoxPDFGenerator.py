da7ef7959a8e281116352e5bebd7cd8ac6cf27a9
AaronSandhu <190224697@aston.ac.uk>
2022-11-27 16:46:07 +0000

    image viewing

--- VevoxPDFGenerator/VevoxPDFGenerator.py
+++ VevoxPDFGenerator/VevoxPDFGenerator.py
@@ -3,7 +3,11 @@ import sys
 import json
 import zipfile
 import pdfkit
-#from PIL import Image
+from PIL import Image
+from io import StringIO
+import numpy as np
+import cv2
+
 
 path_wkhtmltopdf = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
 config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
@@ -16,6 +20,20 @@ zfile = zipfile.ZipFile(sys.argv[1])
 
 poll_file = zfile.open('polls.json')
 poll_data = json.load(poll_file)
+
+img  = zfile.read("resources/1cf6875b-75f6-4a6d-b4db-868c17bfe10d.12567.png")
+tmp1 = cv2.imdecode(np.frombuffer(img, np.uint8), 1) 
+cv2.imshow("image", tmp1)
+print(tmp1)
+ 
+# Maintain output window utill
+# user presses a key
+cv2.waitKey(0)       
+ 
+# Destroying present windows on screen
+cv2.destroyAllWindows()
+#img = Image.open(img)
+#print(img.size, img.mode, len(img.getdata()))
 
 #image_folder = zfile.open('resources/')
 #imlist = zfile.infolist()
@@ -31,11 +49,10 @@ html = ''
 for question_number, question in enumerate(poll_data):
     print(question_number+1, question['@type'], question['text'], question['image'])
     html += f"<h2>{question['@type']}</h2>"
-    html += f"<h3>{question_number} {question['text']}</h2>"
-    for choices in question['choices']:
-         print(choices['text'], choices['isCorrectAnswer'], choices['image'])
-        
-
-    #html += f"<p>{question['text']}</p>"
+    html += f"<h3>{question_number+1} {question['text']} {question['image']}</h2>"
+    if question['@type'] == "MultipleChoiceQuestion":
+        for choices in question['choices']:
+            print(choices['text'], choices['isCorrectAnswer'], choices['image'])
+            html += f"<h3>{choices['text']} {choices['isCorrectAnswer']} {choices['image']}</h2>"
 
 pdfkit.from_string(html, 'output.pdf', configuration=config)
