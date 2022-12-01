677e1b54997ff35e6c25c2ca333cabd8ce9ce737
AaronSandhu <190224697@aston.ac.uk>
2022-12-01 15:09:33 +0000

    absolute image path working

--- VevoxPDFGenerator/VevoxPDFGenerator.py
+++ VevoxPDFGenerator/VevoxPDFGenerator.py
@@ -1,4 +1,5 @@
 
+from importlib.metadata import requires
 import sys 
 import json
 import zipfile
@@ -7,10 +8,18 @@ from PIL import Image
 from io import StringIO
 import numpy as np
 import cv2
-
+import base64
+from django.template import Context, Template
+import jinja2
+import os
+from os import listdir
+from os.path import isfile, join
+import re
 
 path_wkhtmltopdf = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
 config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+
+
 
 
 print("This is the name of the script ", sys.argv[0])
@@ -18,41 +27,50 @@ print("This is the name of the script ", sys.argv[0])
 #filepath = r"C:\Users\Aaron\source\repos\FinalYearProject\VevoxPDFGenerator\poll_week6_v2.zip"
 zfile = zipfile.ZipFile(sys.argv[1])
 
+for file in zfile.namelist():
+    zfile.extract(file)
+
+
 poll_file = zfile.open('polls.json')
 poll_data = json.load(poll_file)
 
-img  = zfile.read("resources/1cf6875b-75f6-4a6d-b4db-868c17bfe10d.12567.png")
-tmp1 = cv2.imdecode(np.frombuffer(img, np.uint8), 1) 
-cv2.imshow("image", tmp1)
-print(tmp1)
- 
-# Maintain output window utill
-# user presses a key
-cv2.waitKey(0)       
- 
-# Destroying present windows on screen
-cv2.destroyAllWindows()
-#img = Image.open(img)
-#print(img.size, img.mode, len(img.getdata()))
+onlyfiles = [f for f in listdir('resources/') if isfile(join('resources', f))]
+directory = 'resources/'
+#files = os.listdir(directory)
+print(onlyfiles)
+output = [re.sub(r'\s*\.\d+\.', '.', x) for x in onlyfiles]
+print(output)
 
-#image_folder = zfile.open('resources/')
-#imlist = zfile.infolist()
+i = 0
+while i != len(output):
+    os.rename(directory + onlyfiles[i], directory + output[i])
+    i = i + 1
 
-#print(imlist)
-#for f in imlist:
-#    ifile = zfile.open(f)
-#    img = Image.open(ifile)
-#    print(img)
+print(onlyfiles)
+
 
 
 html = ''
 for question_number, question in enumerate(poll_data):
     print(question_number+1, question['@type'], question['text'], question['image'])
+    string1 = "C:/Users/Aaron/source/repos/FinalYearProject/VevoxPDFGenerator/resources/"
+    string2 = ".gif"
+    questionx = question['image']
+    final = "%s%s%s" % (string1, question['image'], string2)
     html += f"<h2>{question['@type']}</h2>"
-    html += f"<h3>{question_number+1} {question['text']} {question['image']}</h2>"
+    #html += f'<img src= "C:/Users/Aaron/source/repos/FinalYearProject/VevoxPDFGenerator/resources/c19cd52c-37fd-4e8f-91dc-e63729c6744a.gif"/>' 
+    print(final)
+    html += f'<img src={final}/>'
+    html += f"<h2>{question_number+1} {question['text']} {question['image']}</h2>"  
     if question['@type'] == "MultipleChoiceQuestion":
         for choices in question['choices']:
             print(choices['text'], choices['isCorrectAnswer'], choices['image'])
-            html += f"<h3>{choices['text']} {choices['isCorrectAnswer']} {choices['image']}</h2>"
+            html += f"<h3>{choices['text']} {choices['isCorrectAnswer']} {choices['image']}</h3>"
+html += f'<img src="C:\\Users\\Aaron\\Downloads\\poll_week6_v2\\resources\\c19cd52c-37fd-4e8f-91dc-e63729c6744a.2871.gif"/>' 
 
-pdfkit.from_string(html, 'output.pdf', configuration=config)
+
+print(final)
+
+pdfkit.from_string(html, 'output.pdf', configuration=config, options={"enable-local-file-access": ""})
+
+
