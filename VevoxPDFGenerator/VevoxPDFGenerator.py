5faf7004903aca681d81973f554a93cea12cbcd5
AaronSandhu <190224697@aston.ac.uk>
2023-01-24 19:32:43 +0000

    early mvp

--- VevoxPDFGenerator/VevoxPDFGenerator.py
+++ VevoxPDFGenerator/VevoxPDFGenerator.py
@@ -1,20 +1,12 @@
-
-from importlib.metadata import requires
 import sys 
 import json
 import zipfile
 import pdfkit
-from PIL import Image
-from io import StringIO
-import numpy as np
-import cv2
-import base64
-from django.template import Context, Template
-import jinja2
 import os
 from os import listdir
 from os.path import isfile, join
 import re
+import shutil
 
 path_wkhtmltopdf = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
 config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
@@ -34,43 +26,86 @@ for file in zfile.namelist():
 poll_file = zfile.open('polls.json')
 poll_data = json.load(poll_file)
 
-onlyfiles = [f for f in listdir('resources/') if isfile(join('resources', f))]
-directory = 'resources/'
+file_list = [f for f in listdir('resources/') if isfile(join('resources', f))]
+image_directory = 'resources/'
 #files = os.listdir(directory)
-print(onlyfiles)
-output = [re.sub(r'\s*\.\d+\.', '.', x) for x in onlyfiles]
-print(output)
+print(file_list)
+new_list = [re.sub(r'\s*\.\d+\.', '.', x) for x in file_list]
+print(new_list)
 
 i = 0
-while i != len(output):
-    os.rename(directory + onlyfiles[i], directory + output[i])
+while i != len(new_list):
+    os.rename(image_directory + file_list[i], image_directory + new_list[i])
     i = i + 1
 
-print(onlyfiles)
+print(new_list)
 
 
+def findImage(name):
+        imagestring = ''
+        if name['image'] != None and any(name['image'] in s for s in new_list):
+            matching = [s for s in new_list if name['image'] in s]
+            imagestring = os.path.dirname(os.path.abspath(__file__)) + "\\resources\\" + str(matching[0])
+        return imagestring
 
 html = ''
+
 for question_number, question in enumerate(poll_data):
-    print(question_number+1, question['@type'], question['text'], question['image'])
-    string1 = "C:/Users/Aaron/source/repos/FinalYearProject/VevoxPDFGenerator/resources/"
-    string2 = ".gif"
-    questionx = question['image']
-    final = "%s%s%s" % (string1, question['image'], string2)
+    #print(question_number+1, question['@type'], question['text'], question['image'])
     html += f"<h2>{question['@type']}</h2>"
-    #html += f'<img src= "C:/Users/Aaron/source/repos/FinalYearProject/VevoxPDFGenerator/resources/c19cd52c-37fd-4e8f-91dc-e63729c6744a.gif"/>' 
-    print(final)
-    html += f'<img src={final}/>'
-    html += f"<h2>{question_number+1} {question['text']} {question['image']}</h2>"  
+    html += f"<h2>{question_number+1} {question['text']} {question['image']}</h2>"
+    question_image = findImage(question)
+    if question['image'] != None:
+           html += f'<img src="{question_image}"/>'
+
     if question['@type'] == "MultipleChoiceQuestion":
         for choices in question['choices']:
             print(choices['text'], choices['isCorrectAnswer'], choices['image'])
             html += f"<h3>{choices['text']} {choices['isCorrectAnswer']} {choices['image']}</h3>"
-html += f'<img src="C:\\Users\\Aaron\\Downloads\\poll_week6_v2\\resources\\c19cd52c-37fd-4e8f-91dc-e63729c6744a.2871.gif"/>' 
+            choices_image = findImage(choices)
+            if choices['image'] != None:
+                html += f'<img src="{choices_image}"/>'
+        html += f"<h3>{question['correctAnswerExplanation']}</h3>"
+
+    if question['@type'] == "ClickMapQuestion":
+        #for options in question['options']:
+        html += f"<h3>{question['options']} {question['correctAnswers']} {question['correctAnswerExplanation']}</h3>"
+
+    if question['@type'] == "OpenTextQuestion":
+        for answers in question['correctAnswers']:
+            html += f"<h3>{answers}</h3>"
+        html += f"<h3>{question['correctAnswerExplanation']}</h3>"
+
+    if question['@type'] == "RankingQuestion":
+        for choices in question['choices']:
+            html += f"<h3>{choices['sequence']} {choices['text']} {choices['image']}</h3>"
+            choices_image = findImage(choices)
+            if choices['image'] != None:
+                html += f'<img src="{choices_image}"/>'
+
+    if question['@type'] == "NumericQuestion":
+        for answers in question['correctAnswers']:
+            html += f"<h3>Min: {answers['min']} <br> Max: {answers['max']}</h3>"
+        html += f"<h3>{question['correctAnswerExplanation']}</h3>"
+
+    #if question['@type'] == "ScatterPlotQuestion":
+    
+#html += f'<img src="C:\\Users\\Aaron\\Downloads\\poll_week6_v2\\resources\\c19cd52c-37fd-4e8f-91dc-e63729c6744a.2871.gif"/>' 
 
 
-print(final)
+file = open('output.html', 'w')
+file.write(html)
+file.close()
 
 pdfkit.from_string(html, 'output.pdf', configuration=config, options={"enable-local-file-access": ""})
 
+shutil.rmtree(image_directory)
 
+
+
+#multiplechoice
+#opentextquestion
+#rankingquestion
+#numericquestion 
+#scatterplotquestion 
+#clickmapquestion
